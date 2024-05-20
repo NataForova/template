c878f46380816dabc5d79073e68960ad52b05ef1
natalia_shchiptsova <natalia_shchiptsova@belkatechnologies.com>
2024-05-20 23:47:24 +0300

    added client

--- src/main/java/org/greekleanersinc/servicetemplate/ServiceTemplateApplication.java
+++ src/main/java/org/greekleanersinc/servicetemplate/ServiceTemplateApplication.java
@@ -1,13 +1,17 @@
 package org.greekleanersinc.servicetemplate;
 
+import org.greekleanersinc.servicetemplate.client.ClientClient;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class ServiceTemplateApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ServiceTemplateApplication.class, args);
+        ApplicationContext applicationContext = SpringApplication.run(ServiceTemplateApplication.class, args);
+        ClientClient templateTest = applicationContext.getBean(ClientClient.class);
+        templateTest.findTemplate(1L);
     }
 
 }
--- src/main/java/org/greekleanersinc/servicetemplate/client/ClientClient.java
+++ src/main/java/org/greekleanersinc/servicetemplate/client/ClientClient.java
@@ -1,27 +1,27 @@
-/*
 package org.greekleanersinc.servicetemplate.client;
 
-
+import lombok.extern.slf4j.Slf4j;
 import net.devh.boot.grpc.client.inject.GrpcClient;
 import org.greekleanersinc.servicetemplate.Request;
 import org.greekleanersinc.servicetemplate.Response;
 import org.greekleanersinc.servicetemplate.TemplateServiceGrpc;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 @Service
+@Slf4j
 public class ClientClient {
-    private static final Logger logger = LoggerFactory.getLogger(ClientClient.class);
 
     @GrpcClient("grpc-server")
     private TemplateServiceGrpc.TemplateServiceBlockingStub templateServiceStub;
 
-    public void findTemplate(Long id, String message) {
+    public ClientClient() {
+    }
+
+    public void findTemplate(Long id) {
         Request request = Request.newBuilder().setId(id)
                 .build();
         Response helloWorldResponse = this.templateServiceStub.findTemplate(request);
-        logger.info(String.format("Server sent a response: %1s", helloWorldResponse.getResponseData()));
+        log.info(String.format("Server sent a response: %1s", helloWorldResponse.getResponseData()));
     }
 
-}*/
+}
