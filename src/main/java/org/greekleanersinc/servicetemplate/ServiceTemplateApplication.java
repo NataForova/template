23cc0370a0133ec730744744ae7cbda0a7167d5c
natalia_shchiptsova <natalia_shchiptsova@belkatechnologies.com>
2024-07-27 17:21:37 +0300

    remove client from module

--- src/main/java/org/greekleanersinc/servicetemplate/ServiceTemplateApplication.java
+++ src/main/java/org/greekleanersinc/servicetemplate/ServiceTemplateApplication.java
@@ -1,17 +1,12 @@
 package org.greekleanersinc.servicetemplate;
 
-import org.greekleanersinc.servicetemplate.client.TemplateGrpcClient;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class ServiceTemplateApplication {
 
     public static void main(String[] args) {
-        ApplicationContext applicationContext = SpringApplication.run(ServiceTemplateApplication.class, args);
-        TemplateGrpcClient templateTest = applicationContext.getBean(TemplateGrpcClient.class);
-        System.out.println("Client stared");
-        templateTest.findTemplate(1L);
+       SpringApplication.run(ServiceTemplateApplication.class, args);
     }
 }
